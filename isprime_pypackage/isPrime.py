e0d48b334ba98e1ac38a8f771bfadea3692af995
Arslan909 <Arslan909@gmail.com>
2022-07-18 19:44:43 +0500

    removed check variable from code

--- isprime_pypackage/isPrime.py
+++ isprime_pypackage/isPrime.py
@@ -1,5 +1,4 @@
 def isPrime(number):
-    check = 0
     if number==2:
         return True
     if number <= 1 or  number%2==0:
@@ -9,7 +8,6 @@ def isPrime(number):
     i=3
     while i*i<=number:
         if number % i == 0:
-            check = 1
             return False
         i+=2
     return True
