015bbea5f8b81ac1deb45856ea531300c1f20bc6
Arslan909 <Arslan909@gmail.com>
2022-07-18 19:02:36 +0500

    refactored code in is Prime file removed even number check for the prime number

--- isprime_pypackage/isPrime.py
+++ isprime_pypackage/isPrime.py
@@ -1,8 +1,15 @@
 def isPrime(number):
-    if number > 1:
-        for i in range(2,number):  
-            if (number % i) == 0:  
-                return False
+    check = 0
+    if number==2:
         return True
-    else:
+    if number <= 1 or  number%2==0:
+    
         return False
+    
+    i=3
+    while i*i<=number:
+        if number % i == 0:
+            check = 1
+            return False
+        i+=2
+    return True
