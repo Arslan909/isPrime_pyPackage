10fb6d238e93e46027fe4899eaceff524841a1e4
Arslan909 <Arslan909@gmail.com>
2022-07-18 05:18:45 +0500

    refactor the code in IsPrime file

--- isprime_pypackage/isPrime.py
+++ isprime_pypackage/isPrime.py
@@ -1,14 +1,8 @@
 def isPrime(number):
-    check = False
-    if number <= 1:
+    if number > 1:
+        for i in range(2,number):  
+            if (number % i) == 0:  
+                return False
+        return True
+    else:
         return False
-    else:
-        for i in range (2, number):
-            if number % i == 0:
-                check = True
-                break
-        
-    if check == True:
-        return False # return False if number is not prime
-    else:
-        return True # return True if number is prime
